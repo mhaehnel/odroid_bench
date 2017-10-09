f2f4fb802282ac598039f9c9cace4ebfbcb61681
Marcus Haehnel <mhaehnel@tudos.org>
2017-10-09 09:57:06 +0000

    Suppress coproc warning

--- busymc.sh
+++ busymc.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+
 startt=`date +%s%N`
 endt=$(echo $startt+$1*1000000000 | bc -l)
 shift
 taskset -c -p $1 $$
 shift
+
+#suppress coproc warning
+exec 3>&2
+exec 2>/dev/null
+
 for c in $@; do
 	coproc ( taskset -c $c sh -c 'while true; do true; done'; )
 done
+#Undo supression
+exec 2>&3
+
+
 while [ `date +%s%N` -lt  $endt ]; do 
 	true;
 done
